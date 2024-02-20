2dafdef247373d0088268618525b1561cde01dc4
jasonpeacock25 <jasonpeacock25@yahoo.ca>
2024-02-20 10:23:24 -0800

    demo07 completed

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyD_-k3kfVg7LYwiqoGWHiH-k9T0-0F1dYI",
+    authDomain: "comp1800-3ef66.firebaseapp.com",
+    projectId: "comp1800-3ef66",
+    storageBucket: "comp1800-3ef66.appspot.com",
+    messagingSenderId: "423963629519",
+    appId: "1:423963629519:web:9bb826efc698bae28064ba"
 };
 
 //--------------------------------------------

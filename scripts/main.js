d309cd354696fefd5616487e855edcedc86f20aa
jasonpeacock25 <jasonpeacock25@yahoo.ca>
2024-03-05 23:19:48 -0800

    Demo09 part 5 completed

--- scripts/main.js
+++ scripts/main.js
@@ -51,6 +51,7 @@ function displayCardsDynamically(collection) {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
             allHikes.forEach(doc => { //iterate thru each doc
                 var title = doc.data().name;       // get value of the "name" key
+                var docID = doc.id; //reads document id
                 var details = doc.data().details;  // get value of the "details" key
 								var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
                 var hikeLength = doc.data().length; //gets the length field
@@ -61,6 +62,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID; //allows us to activate read more button
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
